18bc82ea71f513c8a572b427b43241fc1596a145
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-09-28 20:47:14 +0530

    Initial & Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./sass/main.scss";
+
+// Components
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector("body").classList.add("loading")
+      : document.querySelector("body").classList.remove("loading");
+  }, [loading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence>
+      {loading ? (
+        <motion.div key="loader">
+          <Loader setLoading={setLoading} />
+        </motion.div>
+      ) : (
+        <>
+          <Header />
+          <Banner />
+          {!loading && (
+            <div className="transition-image final">
+              <motion.img
+                transition={{ ease: [0.6, 0.01, 0.05, 0.9], duration: 5 }}
+                src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
+                layoutId="main-image-1"
+              />
+            </div>
+          )}
+        </>
+      )}
+    </AnimatePresence>
   );
 }
 
